93c73e22412e4555f83120aedd68d23ef0183fb4
Victor Sanchez Gutierrez <mexicanoreos97@gmail.com>
2019-06-08 17:58:53 -0700

    Added Variable, Member Access, Method Call

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -17,12 +17,59 @@ void p(std::string a){
     std::cout << a << std::endl;
 }
 
+bool localVar(CodeGenerator *cg, std::string id) {
+    return cg->currentMethodInfo.variables->find(id) != cg->currentMethodInfo.variables->end();
+}
+
+VariableInfo getMemberVariableInfo(CodeGenerator *cg, std::string id, std::string className) {
+    do {
+        auto *members = cg->classTable->at(className).members;
+        if (members->find(id) != members->end()) {
+            return members->at(id);
+        }
+    } while ((className = cg->classTable->at(className).superClassName) != "");
+    
+    std::cout << "#### MEMBER NOT DEFINED: " << id << std::endl;
+}
+
+VariableInfo getLocalVariableInfo(CodeGenerator *cg, std::string id) {
+    // Variable exists as parameter or local
+    if (cg->currentMethodInfo.variables->find(id) != cg->currentMethodInfo.variables->end()) {
+        return cg->currentMethodInfo.variables->at(id);
+    }
+     // Variable exists as member of class or superclass 
+    else {
+        return getMemberVariableInfo(cg, id, cg->currentClassName);
+    }
+}
+
+
+int getMemberOffset(CodeGenerator *cg, std::string member, std::string className) {
+    auto classInfo = cg->classTable->at(className);
+    // Get the class this member was inherited from otherwise the offset will be wrong
+    while (classInfo.members->find(member) == classInfo.members->end()) {
+        className = classInfo.superClassName;
+        classInfo = cg->classTable->at(classInfo.superClassName);
+    }
+    
+    // Offset the offset by the combined size of all the superclasses (The symbol table doesn't do this automatically)
+    int superClassOffset;
+    if(classInfo.superClassName != "") {
+      superClassOffset = classSize(cg, cg->classTable->at(classInfo.superClassName));  
+    }
+    else {
+        superClassOffset = 0;
+    } 
+    VariableInfo memberInfo = getMemberVariableInfo(cg, member, className);
+    return memberInfo.offset + superClassOffset;
+}
+
 // End of helper functions
 
 void CodeGenerator::visitProgramNode(ProgramNode* node) {
     // WRITEME: Replace with code if necessary
     p(".data");
-    p("printstr: .asciz \"%d\\n\"")
+    p("printstr: .asciz \"%d\\n\"");
     p("");
     p(".text");
     p(".globl Main_main");
@@ -74,6 +121,7 @@ void CodeGenerator::visitAssignmentNode(AssignmentNode* node) {
 
 void CodeGenerator::visitCallNode(CallNode* node) {
     // WRITEME: Replace with code if necessary
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitIfElseNode(IfElseNode* node) {
@@ -128,6 +176,8 @@ void CodeGenerator::visitWhileNode(WhileNode* node) {
     std::cout << "   je " << startLoopLabel << std::endl;
     std::cout << endLabel << ":" << std::endl;
 
+    p("#### END OF WHILE");
+
 }
 
 void CodeGenerator::visitPrintNode(PrintNode* node) {
@@ -157,6 +207,8 @@ void CodeGenerator::visitDoWhileNode(DoWhileNode* node) {
     std::cout << "   cmp  %eax, %ebx" << std::endl;
     std::cout << "   jne " << whileTrueLabel << std::endl;
     std::cout << endLabel << ":" << std::endl;
+
+    std::cout << "#### END OF DO WHILE" << std::endl;
 }
 
 void CodeGenerator::visitPlusNode(PlusNode* node) {
@@ -309,26 +361,209 @@ void CodeGenerator::visitNegationNode(NegationNode* node) {
 
 void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
     // WRITEME: Replace with code if necessary
+    std::cout << "#### METHOD CALL NODE" << std::endl;
+    // Save registers
+    std::cout << "   push %eax" << std::endl;
+    std::cout << "   push %ecx" << std::endl;
+    std::cout << "   push %edx" << std::endl;
+    
+    // Load arguments __cdecl
+    if (node->expression_list) {
+        for (auto argument = node->expression_list->rbegin(); argument != node->expression_list->rend(); ++argument) {
+            (*argument)->accept(this);
+        }
+    }
+
+    //If class method then push class self reference to stack
+    if(node->identifier_2) {
+        std::string className = node->identifier_1->name;
+        VariableInfo classInfo = getLocalVariableInfo(this, className);
+        if(localVar(this, className)) {
+            std::cout << "   movl " << classInfo.offset << "(%ebp), %eax" << std::endl;
+            std::cout << "   push %eax" << std::endl;
+        }
+        //Object is in self
+        else {
+            int classOffset = getMemberOffset(this, className, currentClassName);
+            std::cout << "   movl 8(%ebp), %eax" << std::endl; // Load self
+            std::cout << "   movl " << classOffset << "(%eax), %eax" << std::endl; // Load object
+            std::cout << "   push %eax" << std::endl; // Push object as self for next stackframe
+        }
+    }
+    // Solo Case c0(args)
+    else {
+        if(currentClassName != "Main") {
+            if (currentClassName != "Main") {
+                std::cout << "   movl 8(%ebp), %eax" << std::endl;
+                std::cout << "   push %eax" << std::endl;
+            } 
+            else {
+                std::cout << "   add $-4, %esp" << std::endl; // Empty space for self to work with offsets
+            }
+        }
+    }
+
+    // This determines the method label (Which could be in a superClass)
+    std::string className, methodName;
+    ClassInfo classInfo;
+    if(node->identifier_2) {
+        className = getLocalVariableInfo(this, node->identifier_1->name).type.objectClassName ;
+    } 
+    else {
+        className = currentClassName;
+    }
+
+    if(node->identifier_2) {
+        classInfo = classTable->at(getLocalVariableInfo(this, node->identifier_1->name).type.objectClassName);
+    } 
+    else {
+        classInfo = currentClassInfo;
+    }
+
+    if(node->identifier_2) {
+        methodName = node->identifier_2->name;
+    } 
+    else {
+        methodName = node->identifier_1->name;
+    }
+    while (classInfo.methods->find(methodName) == classInfo.methods->end()) { // While you haven't found a class containing the method
+        className = classInfo.superClassName;
+        classInfo = classTable->at(classInfo.superClassName);
+    }
+
+    // Call the function (Pushes return address to stack then jumps to label)
+    std::cout << "   call " << className << "_" << methodName << std::endl;
+    
+    // Store return value in ebx because we're going to restore eax
+    std::cout << "   movl %eax, %ebx" << std::endl;
+    
+    // Clear arguments and self from stack
+    std::cout << "   add $" << 4 * (node->expression_list->size() + 1) << ", %esp" << std::endl;
+    
+    // Restore registers
+    std::cout << "   pop  %edx" << std::endl;
+    std::cout << "   pop  %ecx" << std::endl;
+    std::cout << "   pop  %eax" << std::endl;
+    
+    // Put return value on the stack
+    std::cout << "   push %ebx" << std::endl;
+
+    std::cout << "#### END OF METHOD CALL" << std::endl;
+
 }
 
 void CodeGenerator::visitMemberAccessNode(MemberAccessNode* node) {
     // WRITEME: Replace with code if necessary
+    std::string className = node->identifier_1->name;
+    std::string varName = node->identifier_2->name;
+    std::cout << "#### MEMBER ACCESS : " << className << "." << varName << std::endl;
+
+    const VariableInfo classInfo = getLocalVariableInfo(this, className);
+    // Class is a local var or parameter
+    if(localVar(this, className)) {
+        const int memberOffset = getMemberOffset(this, varName, classInfo.type.objectClassName);
+        // Load object address into accumulator
+        std::cout << "   movl " << classInfo.offset << "(%ebp), %eax" << std::endl; 
+        // Load object member into accumulator
+        std::cout << "   movl " << memberOffset << "(%eax), %eax" << std::endl; 
+        std::cout << "   push %eax" << std::endl;
+    }
+    // Implicitly in self (Kind of confusing because you need to access the object from self then the member from that object)
+    else {
+        int classOffset = getMemberOffset(this, className, currentClassName);
+        int memberOffset = getMemberOffset(this, varName, classInfo.type.objectClassName);
+        // Load self address into accumulator
+        std::cout << "   movl " << "8(%ebp), %eax" << std::endl; 
+        // Load object address into accumulator
+        std::cout << "   movl " << classOffset << "(%eax), %eax" << std::endl; 
+        // Load member into accumulator
+        std::cout << "   movl " << memberOffset << "(%eax), %eax" << std::endl; 
+        std::cout << "   push %eax" << std::endl;
+    }
+
+    p("#### END OF MEMBER ACCESS");
 }
 
 void CodeGenerator::visitVariableNode(VariableNode* node) {
     // WRITEME: Replace with code if necessary
+    std::string varName = node->identifier->name;
+    std::string className = this->currentClassName;
+    p("#### VARIABLE NODE: " + varName);
+    VariableInfo v;
+    // Should be a parameter or local var
+    if(localVar(this, varName)) {
+        VariableInfo v = getLocalVariableInfo(this, varName);
+        std::cout << "   movl " << v.offset << "(%ebp), %eax" << std::endl;
+        std::cout << "   push %eax" << std::endl;
+    }
+    // Class Member Variable
+    else {
+        int memberOffset = getMemberOffset(this, varName, currentClassName);
+        // Load self address into accumulator
+        std::cout << "   movl " << "8(%ebp), %eax" << std::endl; 
+        // Load object member into accumulator
+        std::cout << "   movl " << memberOffset << "(%eax), %eax" << std::endl; 
+        std::cout << "   push %eax" << std::endl;
+    }
+
+    p("#### END OF VARIABLE NODE");
 }
 
 void CodeGenerator::visitIntegerLiteralNode(IntegerLiteralNode* node) {
     // WRITEME: Replace with code if necessary
+    std::cout << "#### INT LIT" << std::endl;
+    std::cout << "   push " << '$' << node->integer->value << std::endl;
+    std::cout << "#### END INT LIT" << std::endl;
 }
 
 void CodeGenerator::visitBooleanLiteralNode(BooleanLiteralNode* node) {
     // WRITEME: Replace with code if necessary
+    std::cout << "#### BOOL LIT" << std::endl;
+    std::cout << "    push " << '$' << node->integer->value << std::endl;
+    std::cout << "#### END OF BOOL LIT" << std::endl;
 }
 
 void CodeGenerator::visitNewNode(NewNode* node) {
     // WRITEME: Replace with code if necessary
+    std::cout << "#### NEW OPERATOR" << std::endl;
+    auto classInfo = classTable->at(node->identifier->name);
+    std::cout << "   push $" << classSize(this, classInfo) << std::endl;
+    std::cout << "   call malloc" << std::endl;
+    std::cout << "   add  $4, %esp" << std::endl;
+    std::cout << "   push %eax" << std::endl;
+    
+    if (classInfo.methods->find(node->identifier->name) != classInfo.methods->end()) {
+        std::cout << "#### CALLING CONSTRUCTOR" << std::endl;
+        // Save registers
+        std::cout << "   push %eax" << std::endl;
+        std::cout << "   push %ecx" << std::endl;
+        std::cout << "   push %edx" << std::endl;
+        
+        // Load arguments in C_decl (Can't call visit children, arguments will be in reverse order)
+        if (node->expression_list) {
+            for (auto argument = node->expression_list->rbegin(); argument != node->expression_list->rend(); ++argument) {
+                (*argument)->accept(this);
+            }
+        }
+        
+        // Push self
+        std::cout << "   movl " << 4 * (node->expression_list->size()+3) << "(%esp), %eax" << std::endl;
+        std::cout << "   push %eax" << std::endl;
+        
+        // Call constructor
+        std::cout << "   call " << node->identifier->name << "_" << node->identifier->name << std::endl;
+        
+        // Clear arguments and self from stack
+        std::cout << "   add $" << 4 * (node->expression_list->size() + 1) << ", %esp" << std::endl;
+        
+        // Restore registers
+        std::cout << "   pop  %edx" << std::endl;
+        std::cout << "   pop  %ecx" << std::endl;
+        std::cout << "   pop  %eax" << std::endl;
+        
+    }
+
+    std::cout << "#### END OF NEW" << std::endl;
 }
 
 void CodeGenerator::visitIntegerTypeNode(IntegerTypeNode* node) {
